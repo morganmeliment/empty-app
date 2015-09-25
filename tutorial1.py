e5ac14bdfad9f61cd5ae99c2608a1179774c56b9
Morgan Meliment <morganmeliment@gmail.com>
2015-09-24 21:29:01 -0400

    Updated from Brython Server: September 24, 2015 9:29:00 PM EDT

--- tutorial1.py
+++ tutorial1.py
@@ -11,9 +11,10 @@ black = Color(0x000000, 1.0)
 thinline = LineStyle(1, black)
 # A graphics asset that represents a rectangle
 rectangle = RectangleAsset(50, 20, thinline, blue)
-
+rectangle2 = RectangleAsset(50, 20, thinline, blue)
 # Now display a rectangle
 Sprite(rectangle)
+Sprite(rectangle, (10, 10))
 
 myapp = App()
 myapp.run()
